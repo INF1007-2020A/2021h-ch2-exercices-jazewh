2a1a40d683cdac535b1deaf6b8383dc5e577901c
jazewh <77996432+jazewh@users.noreply.github.com>
2021-01-25 17:27:58 -0500

    mot.upper()

--- exercice.py
+++ exercice.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    char(mot)
-    return mot
+    # TODO completer la fonction
+    return mot.upper()
 
 
 if __name__ == '__main__':
